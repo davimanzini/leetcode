2fe403b496e99d478bdfd49ace855829643080f5
Davi Manzini <davi.crdm@gmail.com>
2026-04-20 17:46:03 -0300

    OPTIMUM daily painted houses

---
+++ cpp/OPTtwofurthesthouses.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        
+        int i = 0;
+        int j = colors.size() - 1;
+        int max_dist;
+
+        for(int k = 0; k < colors.size(); k++){
+            if(colors[i] != colors[k]){
+                int curr_dist = abs(k - i);
+                if(curr_dist > max_dist){
+                    max_dist = curr_dist;
+                }
+            }
+            if(colors[j] != colors[k]){
+                int curr_dist = abs(k - j);
+                if(curr_dist > max_dist){
+                    max_dist = curr_dist;
+                }
+            }
+        }
+        return max_dist;
+    }
+};
