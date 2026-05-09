1418e81ba3fe4e97f1ef06adf5debd7039e1bbc9
Davi Manzini <davi.crdm@gmail.com>
2026-05-09 13:41:37 -0300

    Add files via upload

--- cpp/permutations.cpp
+++ cpp/permutations.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+class Solution {
+public:
+
+    void backtrack(vector<vector<int>> &perms, vector<int> nums, vector<int> &curr){
+
+        int n = nums.size();
+
+        if(curr.size() == n){ //tamanho de retorno
+                perms.push_back(curr);
+                return;
+        }
+
+        for(int x : nums){
+            
+            curr.push_back(x);
+        }
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>> perms;
+        vector<int> curr;
+        backtrack(perms, nums, curr);
+        return perms;
+    }
+};
