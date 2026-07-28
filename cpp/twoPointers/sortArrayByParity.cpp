5277b92cc9b7341ba31e27b86e99f18ebd16b8e5
Davi Manzini <davi.crdm@gmail.com>
2026-07-28 20:38:31 -0300

    easy sorting with two pointers

---
+++ cpp/twoPointers/sortArrayByParity.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j){
+            int currLeft = nums[i];
+            int currRight = nums[j];
+            if((currLeft % 2 != 0) && (currRight % 2 == 0)){
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+            if(currLeft % 2 == 0){
+                if(currRight % 2 != 0) j--;
+                i++;
+            }
+            else j--;
+        }
+        return nums;
+    }
+};
